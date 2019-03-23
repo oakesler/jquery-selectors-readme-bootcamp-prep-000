f374bcd56154b464aed6c8705b587e60855a415d
Olivia Kesler <oakesler@protonmail.ch>
2019-03-23 16:41:18 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+
+$(document).ready(function(){
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('div:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul pic-list:first-child') 
+}
+
+});
